04ca52152fcadc98661b8dcba15f41cdae5075da
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-10-22 23:42:46 +0330

    - Create btnExit_Click Method In MainWindow.CS

--- AdminPanelUi/MainWindow.xaml.cs
+++ AdminPanelUi/MainWindow.xaml.cs
@@ -33,5 +33,9 @@ namespace AdminPanelUi
             }
         }
 
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
     }
 }
